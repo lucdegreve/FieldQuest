23d76707acf487873e65618faf73074cca32f32a
Unknown <julien.louet.hallo@hotmail.fr>
2019-01-28 14:28:53 +0100

    test_julien

---
+++ ex2.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<META charset="UTF-8">
+	</head>
+	
+	<body>
+	<?php
+	$nom=Dupont;
+	$prenom=Louis;
+	echo $nom;
+	echo $prenom;
+	"<br/>"
+	echo $nom ." ".$prenom.;
+	"<br/>"
+	?>
+	</body>
+</html>
+	
+	
