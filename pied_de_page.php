8fa068399c350afbf53a7a122ba86d7ad389de11
Antoine <antoine.autran.pro@gmail.com>
2019-02-12 16:32:37 +0100

    V1 en tête et pied de page

---
+++ pied_de_page.php
@@ -0,0 +1,21 @@
+<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<link rel="stylesheet" href="css/custom.css">
+		</head>
+
+		<body>
+				<footer class="footer bg-primary">
+					<div class="container">
+					
+						<span class="text-dark">Copyright AgroTIC and Geosys.
+						</span>
+						<button class="btn btn-primary float-right" onclick="location.href='https://www.geosys.com/fr/'" >
+						<img src="picto\geosys_logo.png" class="rounded float-right" alt="Responsive image" width="100px" height ="50px">
+						</button>
+					</div>
+				</footer>
+		</body>
+</html>
