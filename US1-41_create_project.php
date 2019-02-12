61c97a16f6abd6c9f36608b703176ff2b7c830a2
Ophelie <ophelie.woon-shoo-tong@agro-bordeaux.fr>
2019-02-12 15:49:20 +0100

    Added query to modify a project + deleted unused functions.php

--- US1-41_create_project.php
+++ US1-41_create_project.php
@@ -3,7 +3,7 @@
 <!-----------------------------------------------------------
        US1-41 Create a project 
 Developped by Diane and Ophélie			      
-This page contains the form to create a new project.
+This page contains the form to create a new project or to modify a project.
 
 
 Input variables : 		$id_project
@@ -37,26 +37,26 @@ $result_status = pg_query($connex, "SELECT id_status as stat, id_status, label_s
 $table_status = new Tab_donnees($result_status,"PG");
 
 
-if (isset($_POST['id_project'])){	// Get the id of the project to modify and the informations related to the project
-$id_project = $_POST['id_project'];	
+if (!isset($_POST['id_project'])){	// Get the id of the project to modify and the informations related to the project
+	//$id_project = $_POST['id_project'];	
+	$id_project = 1;
+	// Query to get the informations of the project to modify
+	$query_modify_project = "SELECT id_status, name_project, project_description, project_init_date, project_end_date 
+		FROM projects WHERE id_project = '".$id_project."'";
+	$result_modify_project = pg_query($connex, $query_modify_project) or die ('Failed to get project');
+	$row= pg_fetch_array($result_modify_project);
+	$status = $row[0];
+	$name_project = $row[1];
+	$project_desc = $row[2];
+	$init_date = $row[3];
+	$end_date=$row[4];
 
-// Query to get the informations of the project to modify
-$query_modify_project = "SELECT id_status, name_project, project_description, project_init_date, project_end_date 
-	FROM projects WHERE id_project = '".$id_project."'";
-$result_modify_project = pg_query($connex, $query_modify_project) or die ('Failed to get project');
-$row= pg_fetch_array($result_modify_project);
-$status = $row[0];
-$name_project = $row[1];
-$project_desc = $row[2];
-$init_date = $row[3];
-$end_date=$row[4];
-
-} else {		// If a new project is added
-	$status = '';
-	$name_project = '';
-	$project_desc = '';
-	$init_date ='';
-	$end_date='';
+	} else {		// If a new project is added
+		$status = '';
+		$name_project = '';
+		$project_desc = '';
+		$init_date ='';
+		$end_date='';
 }
 ?>
 
@@ -70,7 +70,7 @@ Fields marked with (*) are mandatory
     <tr><td>(*)</td><td>Date of beginning</td>  <td><input type="date" name="begin_date" value="<?php echo $init_date; ?>"></td></tr>
     <tr><td>   </td><td>Date of end</td>        <td><input type="date" name="end_date" value="<?php echo $end_date; ?>"></td></tr>
     <tr><td>   </td><td>Project description</td><td><textarea name="project_desc" rows="3"><?php echo $project_desc; ?></textarea></td></tr>    
-	<tr><td>(*)</td><td>Status</td>             <td><?php $table_status->creer_liste_option_plus ( "status", "id_status", "label_status"); ?></td></tr>
+	<tr><td>(*)</td><td>Status</td>             <td><?php $table_status->creer_liste_option_plus ( "status", "id_status", "label_status",$status); ?></td></tr>
 </table>
 
 <input type='submit' name='validate' value='Validate'>
@@ -79,25 +79,42 @@ Fields marked with (*) are mandatory
 
 <?php	// Validate and add or modify the project 
 if(isset($_POST['validate'])){
-	$project_name = $_POST['project_name'];
-	$project_status = $_POST['status'];
-	$project_desc = $_POST['project_desc'];
-	$project_start = $_POST['begin_date'];
-	echo $project_name.$project_status;
-	
-	if ($_POST['end_date']!=""){
-		$project_end = $_POST['end_date'];
+	if ($id_project!=''){
+		$project_name = $_POST['project_name'];
+		$project_status = $_POST['status'];
+		$project_desc = $_POST['project_desc'];
+		$project_start = $_POST['begin_date'];
+		if ($_POST['end_date']!=""){
+			$project_end = $_POST['end_date'];
+			
+			$query_modify_project = "UPDATE projects SET id_status='".$project_status."', name_project='".$project_name."', project_description='".$project_desc."', 
+			project_init_date='".$project_start."', project_end_date='".$project_end."' WHERE id_project = '".$id_project."'";
+			
+		}else{
+			$query_modify_project = "UPDATE projects SET id_status='".$project_status."', name_project='".$project_name."', project_description='".$project_desc."', 
+			project_init_date='".$project_start."',project_end_date=NULL WHERE id_project = '".$id_project."'";
+		}
+		$result = pg_query($connex, $query_modify_project) or die ('Failed to modify project');
+			echo 'Project modified';
+	} else {
+		$project_name = $_POST['project_name'];
+		$project_status = $_POST['status'];
+		$project_desc = $_POST['project_desc'];
+		$project_start = $_POST['begin_date'];
 		
-		$query_add_project = "INSERT INTO projects (id_status, name_project, project_description, project_init_date, project_end_date)
-			VALUES ('".$project_status."', '".$project_name."', '".$project_desc."', '".$project_start."', '".$project_end."')";
+		if ($_POST['end_date']!=""){
+			$project_end = $_POST['end_date'];
+			
+			$query_add_project = "INSERT INTO projects (id_status, name_project, project_description, project_init_date, project_end_date)
+				VALUES ('".$project_status."', '".$project_name."', '".$project_desc."', '".$project_start."', '".$project_end."')";
+		}else{
+			$query_add_project = "INSERT INTO projects (id_status, name_project, project_description, project_init_date)
+			VALUES ('".$project_status."', '".$project_name."', '".$project_desc."', '".$project_start."')";
+		}
 		$result = pg_query($connex, $query_add_project) or die ('Failed to add project');
-		echo 'Project added';
-	}else{
-		$query_add_project = "INSERT INTO projects (id_status, name_project, project_description, project_init_date)
-		VALUES ('".$project_status."', '".$project_name."', '".$project_desc."', '".$project_start."')";
-		$result = pg_query($connex, $query_add_project) or die ('Failed to add project');
-		echo 'Project added';
+			echo 'Project added';
 	}	
 }
+
 ?>
 </html>
