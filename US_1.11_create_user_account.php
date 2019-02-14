7bfde127891ed5e1f8a5e4278cf8e3da7173c3c3
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2019-02-14 09:20:18 +0100

    MAJ create user account V3
    J'ai commenté le code et ai vérifié deux trois détails

--- US_1.11_create_user_account.php
+++ US_1.11_create_user_account.php
@@ -12,6 +12,8 @@
         NB : All the fields with (*) must be filled
 
         <script type="text/javascript">
+        // Function valider(), verify for each field if it is filled. If not, warning message appears
+
         function valider(){
           var c = 0 ;
           var msg = "" ;
@@ -35,30 +37,34 @@
             alert(msg);
             return false;
           }
-
         }
         </script>
 
-        <form name="account_creation" action="US_1.11_p1.php" onsubmit="return valider()" method="GET">
+        <form name="account_creation" action="US_1.11_create_user_account.php" onsubmit="return valider()" method="GET">
         <br/>
         <?php
-        //$connection = pg_connect("host=localhost port=5432 dbname=base_exo2 user=postgres password=postgres")or die ("Connexion impossible");
+
+        // Connexion to class file and connexion file
         require "tab_donnees/tab_donnees.class.php";
         require "tab_donnees/funct_connex.php";
-
+        // Variables needed for connexion
         $con = new Connex();
         $connex = $con->connection;
 
+        // Query to get all information from user_type to make a list of user type.
+        // With this admin will choose between three possibilities what type of user the account is for
         $result= pg_query($connex, "SELECT * FROM user_type");
         $tab = new Tab_donnees($result,"PG");
 
         ?>
 
         <table>
+        <!-- Here is a table of all the fields admin have to fill -->
         <!-- Attention à l'identifiant utilisateur ?? -->
         <tr>
         <td> (*) User type </td><td>
           <?php
+                // Here is the list of user types
                 $tab->creer_liste_option_plus ("list_user_type", "id_user_type", "name_user_type");
           ?>
         </td>
@@ -98,6 +104,7 @@
         <input type="submit" name="createaccount" value="Create account">
         </form>
 
+        <!-- After submitting this form, issset verify first name and last name are filled, if so, data are loaded in the database -->
         <?php
         if (isset($_GET["last_name"],$_GET["first_name"]))
         {
@@ -113,11 +120,12 @@
         $phone = $_GET["phone"];
         $website = $_GET["website"];
 
+        // From the first name we want to have the first letter to create a login that is first letter of first name + last name (ccochais for Chloé Cochais for exemple)
         $length_fn = strlen($first_name);
         $substr_fn = substr($first_name, -$length_fn, 1);
 
-        // Génération d'une chaine aléatoire
-        function chaine_aleatoire($nb_car, $chaine = 'azertyuiopqsdfghjklmwxcvbn123456789')
+        // Function generating a password
+        function password($nb_car, $chaine = 'azertyuiopqsdfghjklmwxcvbn123456789')
         {
             $nb_lettres = strlen($chaine) - 1;
             $generation = '';
@@ -129,24 +137,19 @@
             }
             return $generation;
         }
-        $password = chaine_aleatoire(12, $chaine = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPMLKJHGFDSQWXCVBN123456789');
 
-        //$dbconn = pg_connect("host=localhost dbname=bdd_geosys user=postgres password=mdpPG2019") or die ("Connexion impossible");
-        // Detail de la requete
+        // Generating a password of 12 caracters
+        $password = password(12, $chaine = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPMLKJHGFDSQWXCVBN123456789');
+
+        // Query to update database
         $query = "INSERT INTO user_account(id_user_type, last_name, first_name, address, postcode, city, country, email, phone, company, website, login, password)
         VALUES ('".$user_type."','".$last_name."','".$first_name."','".$address."',
         '".$postcode."','".$city."','".$country."','".$email."','".$phone."','".$company."','".$website."','".$substr_fn.$last_name."','".$password."')";
-
-        //execute la requete dans le moteur de base de donnees
-        $query_result = pg_query($dbconn,$query) or die (pg_last_error() );
+        $query_result = pg_query($connex,$query) or die (pg_last_error() );
 
         echo "Data have been correctly inserted into database";
       }
         ?>
-
-
-      </body>
-    </html>
 
   </body>
 </html>
