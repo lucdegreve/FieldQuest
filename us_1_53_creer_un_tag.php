22e8729cb70355be8e602c76759508e84a707617
AJambon <aurelie.jambon@agro-bordeaux.fr>
2019-02-14 09:15:23 +0100

    Final version of insert queries for tag and tag type

--- us_1_53_creer_un_tag.php
+++ us_1_53_creer_un_tag.php
@@ -6,18 +6,19 @@
 	</head>
 		<body>
 		<?php 
-		require "tab_donnees.class.php";
-		require "funct_connex.php";
+		require "tab_donnees/tab_donnees.class.php";
+		require "tab_donnees/funct_connex.php";
 		$con = new Connex();
 		$connex = $con->connection;
-		$id_tag=$_GET[""];// Demander à Camille 
+		
 		$id_tag_type=$_GET[""];// Demander à Camille 
-		$name_tag=$_GET[""];//Demander à Camille
-		$tag_description=[""]; //Demander à Camille
-		$sql = 'INSERT INTO tag_type VALUES ("'.$id_tag.'","'.$id_tag_type.'" ,"'.$name_tag.'", "'.$tag_description.'")'; 
-		$result=pg_query($connex,$sql);
-		echo '<script type="text/javascript">';
-			echo'alert("New tag '.$name_tag.' created")';
+		$tag_name=$_GET[""];//Demander à Camille
+		$tag_description=$_GET[""]; //Demander à Camille
+		$sql = "INSERT INTO tags (id_tag_type,tag_name,tag_description) VALUES ('".$id_tag_type."' ,'".$tag_name."','".$tag_description."')"; 
+		echo $sql;
+		$result=pg_query($connex,$sql);//à modifier en fonction de la page connection
 		?>
+		<script type="text/javascript">;
+			alert(<?php echo"New tag $name_tag created";?>);
 		</body>
 </html>
