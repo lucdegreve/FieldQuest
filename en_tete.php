39ca0a8e5bd13fa96ce1db07b32bc6a0adc0ce5c
Ophelie <ophelie.woon-shoo-tong@agro-bordeaux.fr>
2019-02-14 15:46:36 +0100

    Added header and footer files

---
+++ en_tete.php
@@ -0,0 +1,27 @@
+<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<link rel="stylesheet" href="css/custom.css">
+		</head>
+
+		<body>
+			<!--  
+			Fonction pour importer l'en_tete à mettre dans le début du body de votre page
+			<?php
+				 include('.../git/FieldQuest/en_tete.php');
+			?>
+			-->
+				<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+					<button class="btn btn-primary" onclick="location.href='https://www.agro-bordeaux.fr/'" >
+					<img src="picto\back.png" class="img-fluid" alt="Responsive image" width="75px" height ="75px">
+					</button>
+					<img src="picto\geosys_logo.png" class="rounded mx-auto d-block" alt="Responsive image" width="250px" height ="125px">
+					<button class="btn btn-primary" onclick="location.href='https://www.montpellier-supagro.fr/'" >
+					<img src="picto\person.png" class="rounded float-right" alt="Responsive image" width="75px" height ="75px">
+					</button>
+					
+				</nav>
+		</body>
+</html>
