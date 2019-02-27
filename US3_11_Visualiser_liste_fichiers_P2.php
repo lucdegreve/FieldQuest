fa3db0cf7bdcee9a8feefc00fcf3ed01a992c636
Elsa Guilley <elsa.guilley@agro-bordeaux.fr>
2019-02-27 16:16:10 +0100

    Modif requête

--- US3_11_Visualiser_liste_fichiers_P2.php
+++ US3_11_Visualiser_liste_fichiers_P2.php
@@ -17,11 +17,11 @@
 		$con=new Connex();
 		$connex=$con->connection;
 		//Get variable
-		$name=$_GET['name'];
+		$original_id=$_GET['original_id'];
 		//Query
-		$query="SELECT id_file, to_char(upload_date,'DD/MM/YYYY'), file_size, id_version, last_name, first_name
+		$query="SELECT id_file, file_name, to_char(upload_date,'DD/MM/YYYY'), file_size, id_version, last_name, first_name
 		FROM user_account ua JOIN files f ON ua.id_user_account=f.id_user_account JOIN validation_state vs ON f.id_validation_state=vs.id_validation_state
-		WHERE file_name='".$name."'
+		WHERE id_original_file=".$original_id."
 		ORDER BY id_version DESC";
 		$result_versions=pg_query($connex, $query) or die('Échec de la requête : ' . pg_last_error());
 		$nbrows=pg_num_rows($result_versions);
@@ -42,11 +42,12 @@
 					<?php
 					while($col=pg_fetch_array($result_versions)){
 						$id_file=$col[0];
-						$date=$col[1];
-						$size=$col[2];
-						$version=$col[3];
-						$last_name=$col[4];
-						$first_name=$col[5];
+						$name=$col[1];
+						$date=$col[2];
+						$size=$col[3];
+						$version=$col[4];
+						$last_name=$col[5];
+						$first_name=$col[6];
 						echo "<tr class='table-info'>";
 							echo "<td>".$name."</td>";
 							echo "<td>".$date."</td>";
