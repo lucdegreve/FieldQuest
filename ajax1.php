1d4e3de040d3e975808f70039bda6d97d33fa4c9
Gala <galadriel.briere@gmail.com>
2019-02-28 10:53:50 +0100

    Delete old scripts (renamed in last commit)

--- ajax1.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$coords = $_GET['coords'];
-$_SESSION['latitude'] = $coords[0];
-echo $coords[0];
-?>
--- ajax2.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$coords = $_GET['coords'];
-echo $coords[1];
-$_SESSION['longitude'] = $coords[1];
-?>
