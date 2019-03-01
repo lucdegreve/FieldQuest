81193f186d2175e79a6481cd95499173258adbcf
Ophelie <ophelie.woon-shoo-tong@agro-bordeaux.fr>
2019-03-01 15:44:00 +0100

    Changed jquery & bootstrap src for US4-11

--- US4_11_Requete_source_final.php
+++ US4_11_Requete_source_final.php
@@ -8,9 +8,6 @@ ligne 90 url : mettre l'url souhaité-->
 <!DOCTYPE html>
 <html>
  <head>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
