e0bcbb9c62d0660a2167393675b5df029c174d22
Antoine <antoine.autran.pro@gmail.com>
2019-03-01 11:31:59 +0100

    MAJ en tete et menu_administrateur

--- en_tete.php
+++ en_tete.php
@@ -7,16 +7,39 @@
 		</head>
 
 		<body>
-			<!--  
-			Fonction pour importer l'en_tete à mettre dans le début du body de votre page
-			<?php
-				 include('.../git/FieldQuest/en_tete.php');
-			?>
-			-->
+			
 				<nav class="navbar navbar-expand-lg navbar-dark bg-info">
-					<button class="btn btn-info" onclick="location.href='https://www.agro-bordeaux.fr/'" >
+				<?php
+					session_start();
+					$id_user_type=$_SESSION['id_user_type'];
+
+					if ($id_user_type==1) 
+					{
+					?>
+					<button class="btn btn-info" onclick="location.href='US0_page_accueil_admin.php'" >
 					<img src="picto\back.png" class="img-fluid" alt="Responsive image" width="75px" height ="75px">
 					</button>
+					<?php
+					}
+					else if ($id_user_type==2)
+					{
+					?>
+					
+					<button class="btn btn-info" onclick="location.href='US0_page_accueil_externes.php'" >
+					<img src="picto\back.png" class="img-fluid" alt="Responsive image" width="75px" height ="75px">
+					</button>
+					<?php
+					}
+					else if ($id_user_type==3)
+					{
+					?>
+					<button class="btn btn-info" onclick="location.href='US0_page_accueil_internes.php'" >
+					<img src="picto\back.png" class="img-fluid" alt="Responsive image" width="75px" height ="75px">
+					</button>
+					<?php
+					}	
+					?>
+
 					<img src="picto\geosys_logo.png" class="rounded mx-auto d-block" alt="Responsive image" width="250px" height ="125px">
 					<button class="btn btn-info" onclick="location.href='US_1.21_account_monitoring_by_user.php'" >
 					<img src="picto\person.png" class="rounded float-right" alt="Responsive image" width="75px" height ="75px">
