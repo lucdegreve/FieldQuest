8fe39594758a6e71d1da92dde3c4f2ba34e0ea31
Eva <eva.totain@supagro.fr>
2019-03-04 17:17:37 +0100

    Bouton de retour et vérification
    Bouton de retour à la page "gérer mes projets" et demande si on veut vraiment supprimer un projet

--- us_1_43_supprimer_un_projet.php
+++ us_1_43_supprimer_un_projet.php
@@ -9,8 +9,57 @@
 		<?php
 				 include("en_tete.php");
 		?>
+		
 		<?php
-		$id_project= $_GET["id_project"];
+				$id_project= $_GET["id_project"];
+		?>
+		
+		<!-- Message to check if the user really want to delete a project -->
+		<div class="container-fluid">
+		
+			<br/><br/><br/>
+			<div class="row">
+				<div class="col-3">
+				</div>
+				<div class="col-6">
+					<div class="card text-center">
+					  <div class="card-body">
+					    <h5 class="card-title">Do you really want to delete this project <?php echo $id_project; ?> ?</h5>
+					    <p class="card-text"> 
+					    	<div class="row">
+					    		<div class="col-4">
+								</div>
+						    	<div class="col-2">
+						    		<!-- we create a hidden input to keep the value of the id_project once the form is set-->
+								    <form name="suppr" action="us_1_43_supprimer_un_projet.php" method="GET">
+								    	<input type="hidden" name="id_proj" value="<?php echo $id_project; ?>">
+					              		<input type="submit" name="delete" value="Yes">
+					         		</form>
+					         	</div>
+					         	<div class="col-2">
+					         	<!-- we create a button to go back to the page "manage the projects" -->
+					         		<form name="return" action="US1_42_Gerer_projets.php" method="POST">
+					              		<input type="submit" name="return" value="No / Return">
+					         		</form>
+			         			</div>
+			         			<div class="col-4">
+								</div>
+			         		</div>
+					    </p>
+					  </div>
+					</div>
+				</div>
+				<div class="col-3">
+				</div>
+			</div>
+		</div>
+		
+
+		<!-- If the user click on YES -->
+
+		<?php if (isset($_GET['delete'])){
+		
+		$id_project= $_GET['id_proj'];
 		require "tab_donnees/tab_donnees.class.php";
 		require "tab_donnees/funct_connex.php";
 		$con = new Connex();
@@ -21,19 +70,20 @@
 		if($nb_files ==0){
 			$sql = pg_query($connex,'Delete from projects where id_project='.$id_project.'');  // if the project is empty we can delete it
 			echo'The project has been deleted';
-		}
+			}
 		else {
 			echo'<script type="text/javascript">';
 			echo "alert('The project isn't empty and cannot be deleted')";
 			echo'</script>';
+			}
+
 		}
+		
 		?>
 		
-		<form name="return" action="US1_42_Gerer_projets.php" method="POST">
-			              		<input type="submit" name="return" value="Return">
-		</form>
 		<?php
 				 include("pied_de_page.php");
 		?>
-		</body>
+		
+	</body>
 </html>
