cd799db126f295e63fcbd70679fb04e71fd236c9
Elsa Guilley <elsa.guilley@agro-bordeaux.fr>
2019-03-04 17:09:42 +0100

    Modif due à la checkbox

--- US_2_21_insert_bdd.php
+++ US_2_21_insert_bdd.php
@@ -19,6 +19,7 @@ $date=$_GET['daterange'];
 //get file format
 $file_extension = end(explode('.',$file_name));
 
+
 // start&end date from DRP us format
 $start_date = substr($date,0,10);
 $end_date = substr($date,-10,10);
@@ -41,11 +42,10 @@ $ending_date = $dayend."/".$monthend."/".$yearend;
 
 
 //get todays date and turn it into en date
-
-$today_en = date("m/d/y");
+$today_en = date("m/d/y");   
 $todD = substr($today_en,0,2);
 $todM = substr($today_en,3,2);
-$todY = substr($today_en,6,4);
+$todY = substr($today_en,6,4);      
 $today_fr= $todM."/".$todD."/".$todY;                  //date at good format
 
 
@@ -56,9 +56,7 @@ $connex = $con->connection;
 $query = "SELECT id_tag FROM  tags  ";
 $result = pg_query($connex, $query) or die(pg_last_error());
 //for all tags check if tag selected
-
-while ($row = pg_fetch_array($result)) {
-
+while ($row = pg_fetch_array($result)) { 
 	$var=$_GET[$row["id_tag"]."_tag"];
 	if ($var == "on") { //if checkbox checked
 		array_push($all_tags, $row["id_tag"]); //add tag id to array
@@ -66,13 +64,11 @@ while ($row = pg_fetch_array($result)) {
 }
 
 // getting format id
-$query = "SELECT label_format,id_format FROM  format  ";
+$query = "SELECT label_format, id_format FROM format";
 $result = pg_query($connex, $query) or die(pg_last_error());
 
 //finding if format already exists and adding id if so
-
-while ($row = pg_fetch_array($result)) {
-
+while ($row = pg_fetch_array($result)) { 
 	if ($row[0]==$file_extension){
 		$file_format=$row[1];
 		break;
@@ -87,11 +83,9 @@ else{
 		$query = "insert into format (label_format) VALUES('".$file_extension."') ";
 		$result = pg_query($connex, $query) or die(pg_last_error());
 		//finding new id  for this format in DB
-		$query = "SELECT label_format,id_format FROM  format  ";
+		$query = "SELECT label_format, id_format FROM format";
 		$result = pg_query($connex, $query) or die(pg_last_error());
-
-		while ($row = pg_fetch_array($result)) {
-
+		while ($row = pg_fetch_array($result)) { 
 			if ($row[0]==$file_extension){
 				$file_format=$row[1];
 				break;
@@ -99,32 +93,37 @@ else{
 		}
 }
 
+//Get the state
+if(isset($_GET['validated'])){
+	$id_validation_state=2;
+}
+else{
+	$id_validation_state=1;
+}
+
 
 //Test var |||| needs to be changed for real session's variable
+	//$id_user_account et $use_id_user_account doivent être les mêmes lors d'un dépôt !!! /!\
 $id_user_account=1;
 $use_id_user_account=1;
-$id_validation_state=1;
-$id_version=1;
 
 
 // importing in the DB
 
-
-
-		$query = "INSERT INTO files(id_user_account,use_id_user_account,id_format,id_validation_state,id_version,upload_date, file_name, file_comment, data_init_date,data_end_date,latitude,longitude,file_place,file_size)
-
+		
+		
+		$query = "INSERT INTO files(id_user_account,use_id_user_account,id_format,id_validation_state,id_version,upload_date, file_name, file_comment, data_init_date,data_end_date,latitude,longitude,file_place,file_size) 
         VALUES ('".$id_user_account."','".$use_id_user_account."','".$file_format."','".$id_validation_state."',
-        '".$id_version."','".$today_fr."','".$file_name."','".$comment."','".$starting_date."','".$ending_date."','".$latitude."','".$longitude."','".$file_place."','".$file_size."')";
+        1,'".$today_fr."','".$file_name."','".$comment."','".$starting_date."','".$ending_date."','".$latitude."','".$longitude."','".$file_place."','".$file_size."')";
         $query_result = pg_query($connex,$query) or die (pg_last_error() );
 
-
-
+		
+		
 		//getting file's id from DB to put tags into DB
-
+		
 $query = "SELECT id_file FROM  files where file_name='".$file_name."'";
 $result = pg_query($connex, $query) or die(pg_last_error());
-while ($row = pg_fetch_array($result)) {
-
+while ($row = pg_fetch_array($result)) { 
 $id_now=$row[0];
 }
 
@@ -142,27 +141,21 @@ for ($i=0;$i<$nb_tags;$i++){
 }
 
 // insert file and projet link (multiple projects)
-
-if(isset($_GET['projet']) && !empty($_GET['projet'])){
-    $Col1_Array = $_GET['projet'];
-    foreach($Col1_Array as $selectValue){
+if(isset($_GET['projet']) && !empty($_GET['projet'])){ 
+    $Col1_Array = $_GET['projet']; 
+    foreach($Col1_Array as $selectValue){ 
 		$query = "insert into link_file_project (id_file,id_project)
 				VALUES ('".$id_now."','".$selectValue."')";
 		$query_result = pg_query($connex,$query) or die (pg_last_error() );
-    }
-}
-
+    } 
+}	
 ?>
 <br/>
 <div class="container">
 	<h1  align="center">Your file has been imported successfully, thank you !</h1></br>
 </div>
 
-<form action = "US_2_21_dragdrop_index.php" method = "POST" name = "Return">
-<input type = "submit" value = "Return">
-</form>
-
 <?php	include("pied_de_page.php"); ?>
 
 </html>
-
+		
