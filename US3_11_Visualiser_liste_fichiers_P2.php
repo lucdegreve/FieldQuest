608d56fb972e4b861dd2c0025e382916e94845a2
Elsa Guilley <elsa.guilley@agro-bordeaux.fr>
2019-03-04 17:11:52 +0100

    Correction erreur affichage id_user_account

--- US3_11_Visualiser_liste_fichiers_P2.php
+++ US3_11_Visualiser_liste_fichiers_P2.php
@@ -19,8 +19,7 @@
 		//Get variable
 		$original_id=$_GET['original_id'];
 		//Query
-		$query="SELECT id_file, file_name, to_char(upload_date,'DD/MM/YYYY'), file_size, id_version, last_name, first_name
-		FROM user_account ua JOIN files f ON ua.id_user_account=f.id_user_account JOIN validation_state vs ON f.id_validation_state=vs.id_validation_state
+		$query="SELECT id_file, file_name, to_char(upload_date,'DD/MM/YYYY'), file_size, id_version, use_id_user_account FROM files
 		WHERE id_original_file=".$original_id."
 		ORDER BY id_version DESC";
 		$result_versions=pg_query($connex, $query) or die('Échec de la requête : ' . pg_last_error());
@@ -42,14 +41,18 @@
 					<?php
 					while($col=pg_fetch_array($result_versions)){
 						$id_file=$col[0];
-						$name=$col[1];
+						$file_name=$col[1];
 						$date=$col[2];
 						$size=$col[3];
 						$version=$col[4];
-						$last_name=$col[5];
-						$first_name=$col[6];
+						$use_id_user_account=$col[5];
+						$result_editor=pg_query($connex, "SELECT first_name, last_name FROM user_account WHERE id_user_account=".$use_id_user_account) or die('Échec de la requête : ' . pg_last_error());
+						while($name=pg_fetch_array($result_editor)){
+							$first_name=$name[0];
+							$last_name=$name[1];
+						}
 						echo "<tr class='table-info'>";
-							echo "<td>".$name."</td>";
+							echo "<td>".$file_name."</td>";
 							echo "<td>".$date."</td>";
 							echo "<td>".$first_name." ".$last_name."</td>";
 							echo "<td>".$size."</td>";
