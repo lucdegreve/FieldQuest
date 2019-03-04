fcdeb53c7b168dc5eecf1191c5ebac0ef80ba8be
MSJarre <manuel.labous@agro-bordeaux.fr>
2019-03-04 15:42:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- tab_donnees/funct_connex.php
+++ tab_donnees/funct_connex.php
@@ -5,12 +5,7 @@ class Connex
 	public $connection ;
 
      public function __construct(){
-
-		 $this->connection = pg_connect("host=194.199.251.150 port=5432 dbname=fieldquest user=postgres password=postgres")or die ("Connexion impossible");
-
-		    //$this->connection = pg_connect("host=194.199.251.150 port=5432 dbname=fieldquest user=postgres password=postgres")or die ("Connexion impossible");
-         
-
+		 $this->connection = pg_connect("host=localhost port=5432 dbname=fieldquest user=postgres password=postgres")or die ("Connexion impossible");
      }
 	 
 }?>
