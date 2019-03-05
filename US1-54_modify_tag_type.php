4ff932acc657e6d1354f7586eed31af4bb5ff383
julien louet <julien.louet.hallo@hotmail.fr>
2019-03-05 09:16:17 +0100

    page de modification + suppression de catégorie de tags

--- US1-54_modify_tag_type.php
+++ US1-54_modify_tag_type.php
@@ -33,10 +33,18 @@
             }
 
         </script>
+		<?php session_start() ?>
 	</head>
 	<body>
+		<?php
+				 include("en_tete.php");
+        ?>
+        <div class="container"> 
         <?php 
-        require "tab_donnees/funct_connex.php";
+        require "./tab_donnees/funct_connex.php";
+		echo'<form action="US1-54_manage_tags.php">
+				<div style="display:inline"> <input name="return" class="btn btn-outline-info" type="submit" value="return" /></div>
+				</form>';
         echo '<form  method="get">
             <fieldset style="width: 500px"> 
 				<legend>Select the tag type to modify</legend>
@@ -58,16 +66,34 @@
                         echo' value='.$row["id_tag_type"].'>'.$row["name_tag_type"].'</option>';
                     }
                 echo '</select>
-                <div style="display:inline"> <input type="submit" value="Modify"  /></div>
-            </fieldset>
+                <div style="display:inline"> <button type="submit" class="btn btn-outline-warning">Modify</button></div>'
+				?>
+				<div style="display:inline"> <input name="delete" class="btn btn-outline-danger" type="submit" value="Delete" onClick="return confirm('Do you really want to delete the tag?')"  /></div>
+				<?php
+            echo '</fieldset>
         </form>';
+		
+		
+		// if we click on the Validate button of the previous form 
+        //(to send the informations to modify the tag type table)
+        if (isset($_GET["delete"])){
+            $id_tag_type=$_GET["tag_type"];
+            $con=new Connex();
+            $connex=$con->connection;
+            // Request to update the table "tag_type"
+            $query = "DELETE FROM tag_type WHERE id_tag_type='".$id_tag_type."' " ;       
+            // Request execution
+            $result = pg_query($connex, $query)  
+                or die('Échec de la requête : ' . pg_last_error($connex)); 
+            // displays this message if the modification is a success
+            echo 'The tag has been deleted';
+        }
+		
         
         //if we click on the previous "modify" button of the form
         //it displays the form to edit the selected tag type
         //which displays the value of the fields to modify it
         if (isset($_GET["tag_type"])){
-            session_start(); 
-            require_once "tab_donnees/funct_connex.php";
             $con=new Connex();
             $connex=$con->connection;
             $id_tag_type=$_GET["tag_type"];
@@ -76,11 +102,22 @@
             $result3 = pg_query($connex, $query3)or die(pg_last_error($connex));
             while($row3 = pg_fetch_assoc($result3)){
                 echo '<form method="GET" name="edition_tag_type" action="US1-54_modify_tag_type.php" onsubmit="return validation()">
-                Edit the tag type name  :
-                <input type="text" name="name_tag_type" value="'.$row3["name_tag_type"].'" ><br/>
-                Edit the new description:
-                <input type="text" name="description_tag_type" value="'.$row3["description_tag_type"].'" ><br/>
-                <div><input type="submit" value="Validate" /></div>
+                
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Edit the tag type </span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="name_tag_type" value="'.$row3["name_tag_type"].'" ><br/>
+                </div>
+                </br>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Edit the description</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="description_tag_type" value="'.$row3["description_tag_type"].'">
+                </div>
+               
+                <div><button type="submit" class="btn btn-outline-success">Validate</button></div>
                 </form>';
             }
             
@@ -88,14 +125,12 @@
         // if we click on the Validate button of the previous form 
         //(to send the informations to modify the tag type table)
         if (isset($_GET["name_tag_type"])){
-            session_start();
             $id_tag_type= $_SESSION["id_tag_type"];
             $name_tag_type=$_GET["name_tag_type"];
             if (isset($_GET["description_tag_type"]))
                 {$description_tag_type = $_GET["description_tag_type"];
             } 
             else $description_tag_type= " ";
-            require_once "tab_donnees/funct_connex.php";
             $con=new Connex();
             $connex=$con->connection;
             $res = pg_query($connex, "SELECT * FROM tag_type ")or die(pg_last_error($connex));
@@ -103,12 +138,14 @@
             // Request to update the table "tag_type"
             $sql = "UPDATE tag_type SET name_tag_type='".$name_tag_type."' , description_tag_type='".$description_tag_type."' WHERE id_tag_type='".$id_tag_type."' " ;       
             // Request execution   
-            $res = pg_query($connex, $sql)  
-                or die('Échec de la requête : ' . pg_error($connex)); 
+            $res = pg_query($connex, $sql) or die('Échec de la requête : ' . pg_last_error($connex)); 
             // displays this message if the modification is a success
             echo 'The tag type  '.$name_tag_type.' has been modified';
         }
         
 		?>
 	</body>
+	<?php
+		include("pied_de_page.php");
+    ?>
 </html>
