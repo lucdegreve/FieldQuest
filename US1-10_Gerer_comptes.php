d601eb3ad248f38d5690103d91e586ef3ff4c27c
Elsa Guilley <elsa.guilley@agro-bordeaux.fr>
2019-03-07 14:57:01 +0100

    Suppression "back"

--- US1-10_Gerer_comptes.php
+++ US1-10_Gerer_comptes.php
@@ -48,22 +48,8 @@ Not done yet : Link with modify account page
 ?>
 
 	<div class="container">
-		<div class="row">
-			<div class="col-md-1">
-				<?php if($user_type==1){ ?>
-				<form method="GET"  action="US0_page_accueil_admin.php">
-				<?php }
-				if ($user_type==2){ ?>
-				<form method="GET"  action="US0_page_accueil_internes.php">
-				<?php }
-				if($user_type==3){ ?>
-				<form method="GET"  action="US0_page_accueil_externes.php">
-				<?php } ?>
-					<button align="center" type="submit" class="btn btn-primary btn-md">Back</button>
-				</form>
-			</div>
-			
-			<div class="col-md-7"></div>
+		<div class="row">			
+			<div class="col-md-8"></div>
 			<div class="col-md-4">
 				<form name="add_account" action="US_1.11_create_user_account.php" method="GET">
 					<button type='submit' class='btn btn-success btn-block'><B>Add an account</B></button>
