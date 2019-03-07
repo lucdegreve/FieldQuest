3de85a7e08bdcc21bbeb51c3ea9fac86017b9eb0
Diane <calimerodu46@ymail.com>
2019-03-07 14:41:11 +0100

    Add validation to delete a favorite search

--- US4-14_Delete_favorite_query.php
+++ US4-14_Delete_favorite_query.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+
 <!--------------------------------------------------------------------------------
 
    US4-14 Supprimer une recherche enregistrée
@@ -11,12 +12,28 @@ Input variables : 	id_favorite_search
 Output variables :								
             
 ---------------------------------------------------------------------------------->	
-<META charset="utf-8">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/custom.css">
+    
+    <META charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <script type="text/javascript">
+            //Ouvrir la page "US3_4_Supprimer_fichiers_deposes"
+                    function delete_search(id_favorite_search) { 
+                            document.location.href="US4-14_Delete_favorite_query.php?id_favorite_search="+id_favorite_search+"&delete=";
+                    }
+            //Ouvrir la pagde "US4-14_Favorite_query.php"
+                    function back_to_favorite_search() { 
+                            document.location.href="US4-14_Favorite_query.php";
+                    }
+    </script>
+    
 </head>
+
 <body>
+
 <?php
+        
 	// Include header
 	include("en_tete.php");
 	// Include files containing connexion to database
@@ -40,7 +57,19 @@ Output variables :
         
         //if the connected user is the one who made the query, the delete can be done
         if ($id_user_query==$id_user){
-		?>
+            
+		if (isset($_GET['delete'])){
+		
+		$id_favorite_search= $_GET['id_favorite_search'];
+                $query_delete_account = "DELETE FROM favorite_search f WHERE f.id_favorite_search='".$id_favorite_search."'";
+                $result_delete_account = pg_query($connex,$query_delete_account)
+                    or die ("<div class = 'alert alert-danger'>Failed to delete this search</div>");
+                
+                echo "<button type='button' id='btnBack' name='back' class='btn btn-sm btn-success' onclick='return back_to_favorite_search()'>Return</button>";
+                echo '<br/><div class="alert alert-success">This search successfully deleted. </div><br/>';
+                }
+                else {
+                ?>
                 <!-- Message to check if the user really want to delete a search -->
                 <div class="container-fluid">
 		
@@ -51,26 +80,20 @@ Output variables :
 				<div class="col-6">
 					<div class="card text-center">
 					  <div class="card-body">
-					    <h5 class="card-title">Do you really want to delete this search <?php echo $id_project; ?> ?</h5>
+					    <h5 class="card-title">Do you really want to delete this search ?</h5>
 					    <p class="card-text"> 
 					    	<div class="row">
-					    		<div class="col-4">
-								</div>
+					    		<div class="col-3">
+                                                        </div>
 						    	<div class="col-2">
-						    		<!-- we create a hidden input to keep the value of the id_project once the form is set-->
-								    <form name="suppr" action="US4-14_Delete_favorite_query.php" method="GET">
-								    	<input type="hidden" name="id_favorite_search" value="<?php echo $id_favorite_search; ?>">
-					              		<input type="submit" name="delete" value="Yes">
-					         		</form>
+                                                            <button type='button' id='btnDelete' name='delete' class='btn btn-sm btn-success btn-block' onclick='return delete_search("<?php echo $id_favorite_search; ?>")'>Yes</button>
 					         	</div>
-					         	<div class="col-2">
+					         	<div class="col-3">
 					         	<!-- we create a button to go back to the page "manage the projects" -->
-					         		<form name="return" action="US4-11_Main_page_filter.php" method="POST">
-					              		<input type="submit" name="return" value="No / Return">
-					         		</form>
+                                                            <button type='button' id='btnBack' name='back' class='btn btn-sm btn-danger btn-block' onclick='return back_to_favorite_search()'>No / Return</button>
 			         			</div>
 			         			<div class="col-4">
-								</div>
+                                                        </div>
 			         		</div>
 					    </p>
 					  </div>
@@ -82,14 +105,6 @@ Output variables :
 		</div>
                 
                 <?php
-                if (isset($_GET['delete'])){
-		
-		$id_favorite_search= $_GET['id_favorite_search'];
-                $query_delete_account = "DELETE FROM favorite_search f WHERE f.id_favorite_search='".$id_favorite_search."'";
-                $result_delete_account = pg_query($connex,$query_delete_account)
-                    or die ("<div class = 'alert alert-danger'>Failed to delete this search</div>");
-                
-                echo '<br/><div class="alert alert-success">This search successfully deleted. </div>';
                 }
         }
 	
