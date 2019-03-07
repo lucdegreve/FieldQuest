e33ad8837e543268ef838a21a320d9fb2404f0d0
AJambon <aurelie.jambon@agro-bordeaux.fr>
2019-03-07 11:16:31 +0100

    ajout du filtre statut de validation dans save filters

--- US4-11_Save_filters.php
+++ US4-11_Save_filters.php
@@ -17,67 +17,19 @@ Output variables :
 	</head>
 
 	<body>
-	
-<?php
-        //Header
-        include("en_tete.php");
-        echo "</br>";
-        //DB connection
-        require "./tab_donnees/funct_connex.php";
-        require "./tab_donnees/tab_donnees.class.php";
-        $con=new Connex();
-        $connex=$con->connection;
-        
-?>
-
-<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-3"></div>
-			<div class="col-md-6">
-				<div class="card text-white bg-secondary mb-3" style="border-radius: 10px 10px; color: gray">
-					Do you want to save these filters in your favorite request(s) ?
-				</div>
-			</div>
-			<div class="col-md-3"></div>
-		</div>
-	</div>
-
-	
-<!-- data recovery from the selection page -->
-
 		<?php
 
                 if (isset($_POST['start'])){
                 	if ($_POST['start']!=''){
-                        $start_date = $_POST['start']; //only one value, string format 
-                        echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        			echo 'Start date : '.'<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$start_date.'">'.$start_date.'</button>';
-                        			echo '</div></div>'; 
-                        		echo '</div>';
-                        		echo '<div class="col-md-3"></div>';
-                        	echo '</div>';
-                        echo '</div>';
+                        $start_date = $_POST['start'];
+                        echo "Start date : ".$start_date; //only one value, string format                       
                 	}
                 }
 
                 if (isset($_POST['end'])){
                 	if ($_POST['end']!=''){
-                        $end_date = $_POST['end']; //only one value, string format 
-                        echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        			echo 'End date : '.'<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$end_date.'">'.$end_date.'</button>';
-                        			echo '</div></div>'; 
-                        		echo '</div>';
-                        		echo '<div class="col-md-3"></div>';
-                        	echo '</div>';
-                        echo '</div>';
+                        $end_date = $_POST['end'];
+                        echo " </br> End date : ".$end_date; //only one value, string format
                 	}
                 }
                 
@@ -86,129 +38,49 @@ Output variables :
                     foreach ($_POST['format'] AS $i){
                     	$format[] = $i;
                     }
-                    echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        				echo 'Format(s) :';
-                        			foreach($format as $i){
-                        				echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-                        			}
-                        			echo '</div></div>';  
-                        		echo '</div>';
-                        	echo '<div class="col-md-3"></div>';
-                        echo '</div>';
-                    echo '</div>';
+                    echo " </br> Format : ".implode(", ", $format); //array format
                 }
                 
                 if (isset($_POST['projet'])){
                 	$projet = Array();
                     foreach ($_POST['projet'] AS $i){
-                    	$projet[] = $i; //array format
+                    	$projet[] = $i;
                     }
-                	echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        				echo 'Project(s) :';
-                        			foreach($projet as $i){
-                        				echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-                        			}
-                        			echo '</div></div>';  
-                        		echo '</div>';
-                        	echo '<div class="col-md-3"></div>';
-                        echo '</div>';
-                    echo '</div>';
+                	echo " </br> Projet : ".implode(", ", $projet); //array format
+                }
+				
+				if (isset($_POST['Validation_state'])){
+                	$validation_state = Array();
+                    foreach ($_POST['Validation_state'] AS $i){
+                    	$validation_state[] = $i;
+                    }
+                	echo " </br> Validation state : ".implode(", ", $validation_state); //array format
                 }
                 
                 if (isset($_POST['sources'])){
-               		if ($_POST['sources']!='' AND $_POST['sources'] != NULL){
-                        $sources = $_POST['sources']; //string format
+               		if ($_POST['sources']!=''){
+                        $sources = $_POST['sources'];
                 	}
-                	
-					echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        				echo 'Source(s) :';
-                        			while (strlen($sources)>=3){
-				                		$a = strripos($sources, ",");
-				                		$i= substr($sources, $a+1, strlen($sources)); 
-				                		$sources = substr($sources, 0, $a);
-				                		echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-				                	}
-				                	if (strlen($sources)==1){
-				                		$i = substr($sources, -1, 1);
-				                		echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-				                	}
-                        			echo '</div></div>';  
-                        		echo '</div>';
-                        	echo '<div class="col-md-3"></div>';
-                        echo '</div>';
-                    echo '</div>';
+                	echo " </br> Sources : ".$sources; //string format
                 }
                 
                 if (isset($_POST['unit'])){
                 	$unit = Array();
                     foreach ($_POST['unit'] AS $i){
-                    	$unit[] = $i; //array format
+                    	$unit[] = $i;
                     }
-                	echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        				echo 'Unit(s) :';
-                        			foreach($unit as $i){
-                        				echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-                        			}
-                        			echo '</div></div>';  
-                        		echo '</div>';
-                        	echo '<div class="col-md-3"></div>';
-                        echo '</div>';
-                    echo '</div>';
+                    echo " </br> Unit : ".implode(", ", $unit); //array format
                 }
                 
                 if (isset($_POST['tag'])){
                 	$tag = Array();
                     foreach ($_POST['tag'] AS $i){
-                    	$tag[] = $i; //array format
+                    	$tag[] = $i;
                     }
-                	echo '<div class="container-fluid">';
-                        	echo '<div class="row">';
-           						echo '<div class="col-md-3"></div>';
-           						echo '<div class="col-md-6">';
-                        			echo '<div class="card text-white bg-primary mb-3" style="border-radius: 20px 50px"><div class="card-body">';
-                        				echo 'Tag(s) :';
-                        			foreach($tag as $i){
-                        				echo '<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="'.$i.'">'.$i.'</button>';
-                        			}
-                        			echo '</div></div>'; 
-                        		echo '</div>';
-                        	echo '<div class="col-md-3"></div>';
-                        echo '</div>';
-                    echo '</div>';
+                    echo " </br> Tags : ".implode(", ", $tag); //array format
                 }  
           
           ?>
-
-          
-<!-- end of the recovery, beginning of the form -->
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-3"></div>
-			<div class="col-md-6">
-					<!-- Search button -->
-					<button type='submit' class='btn btn_lg btn-success' name='Save in favorite' value="Button1">Save filters</button>
-					<!-- Save button -->
-					<button type='submit' class='btn btn_lg btn-success' name='Return' value="Button2">Return</button>
-			</div>
-			<div class="col-md-3"></div>
-		</div>
-	</div>
 
 	</body>
 	
