064bfa58fdd86fc3d45fe18ff191faaaa21ee43e
Ophelie <ophelie.woon-shoo-tong@agro-bordeaux.fr>
2019-03-07 10:33:06 +0100

    Added query with filters on upload history page

--- US_4_21_access_history.php
+++ US_4_21_access_history.php
@@ -7,14 +7,15 @@
 <?php
 	include("en_tete.php");
 	$id_user=$_SESSION['id_user_account'];
+	
 ?>
 
 
 <!-- Développeur : Eva & Liantsoa
 	Access my deposit history
-	-> writing the query to access the list of my submitted files	
+	-> writing the query to access the list of my submitted files	(changes on query made by Ophélie - query with filters)
 	-> table to display the results of the query
-	-->
+	--> 
 	
 	    <link href="custom.css" rel="stylesheet" type="text/css">
 		<link href="css/boostrap.min.css" rel="stylesheet" type="text/css">
@@ -53,16 +54,102 @@
 
 <?php
 
-require "./tab_donnees/tab_donnees.class.php";
-require "./tab_donnees/funct_connex.php";
+require_once "./tab_donnees/tab_donnees.class.php";
+require_once "./tab_donnees/funct_connex.php";
 
 $con = new Connex();
 $connex = $con->connection;
 
-$query = "SELECT  id_file, file_name, id_format, label_validation_state, id_version, upload_date, file_comment, data_init_date, data_end_date, evaluation_date, evaluation_comment, file_size FROM files JOIN validation_state ON files.id_validation_state = validation_state.id_validation_state WHERE id_user_account=$id_user";
+$query = "SELECT  f.id_file, f.file_name, f.id_format, vs.label_validation_state, f.id_version, f.upload_date, 
+				  f.file_comment, f.data_init_date, f.data_end_date, f.evaluation_date, f.evaluation_comment, f.file_size 
+					FROM files f
+					LEFT JOIN validation_state vs ON f.id_validation_state = vs.id_validation_state 
+					LEFT JOIN link_file_project lfp ON lfp.id_file=f.id_file
+					LEFT JOIN projects p ON lfp.id_project=p.id_project
+					LEFT JOIN format fr ON fr.id_format=f.id_format
+					LEFT JOIN user_account u ON u.id_user_account=f.id_user_account
+					LEFT JOIN link_tag_project ltp ON ltp.id_file=f.id_file
+					LEFT JOIN tags t ON t.id_tag=ltp.id_tag
+				WHERE f.id_user_account = '".$id_user."' AND ";
+// Selected start date 
+	if (isset($_POST['start'])){
+			if ($_POST['start']!=''){
+					$start_date = $_POST['start'];
+
+					$query .= " f.upload_date >'".$start_date."' AND ";
+			}
+	}
+	// Selected end date
+	if (isset($_POST['end'])){
+			if ($_POST['end']!=''){
+					$end_date = $_POST['end'];
+					$query .= " f.upload_date <'".$end_date."' AND ";
+			}
+	}
+
+	// List of selected format
+	if (isset($_POST['format'])){
+			$query .= " f.id_format IN (";
+			foreach ($_POST['format'] AS $i){
+					$query .= $i.", ";
+			}
+			$query = substr($query, 0, strlen($query) -2);
+			$query .= ")";
+			$query .= " AND ";
+	}
+
+	// list of selected projects
+	if (isset($_POST['projet'])){
+		
+			$query .= " lfp.id_project IN (";
+			foreach ($_POST['projet'] AS $i){
+					$query .= $i.", ";
+			}
+			$query = substr($query, 0, strlen($query) -2);
+			$query .= ")";
+			$query .= " AND ";
+			
+	}
+	//TAG_SLD = list of tag selected (units included)
+	$TAG_SLD='(';
+
+	if (isset($_POST['unit'])){
+			foreach ($_POST['unit'] AS $i){
+					$TAG_SLD .= $i.", ";
+			}
+			echo '</br>';
+	}
+
+	if (isset($_POST['tag'])){
+			foreach ($_POST['tag'] AS $i){
+					$TAG_SLD .= $i.", ";
+			}
+			echo '</br>';
+	}
+	// add condition on tag in query if list of tags not empty
+	if ($TAG_SLD!='('){
+			$query .= " ltp.id_tag IN ".$TAG_SLD;
+			$query = substr($query, 0, strlen($query) -2);
+			$query .= ")";
+	}
+
+	//Cut end of query (unecessary WHERE or AND)
+	if (substr($query, -6)=='WHERE '){
+		$query = substr($query, 0, strlen($query) -6);
+	}
+
+	if (substr($query, -4)=='AND '){
+		$query = substr($query, 0, strlen($query) -4);
+	}
+	
+	$query .= " GROUP BY f.id_file, f.file_name, vs.label_validation_state, f.id_version, f.upload_date, 
+	f.file_comment, f.data_init_date, f.data_end_date, f.evaluation_date, f.evaluation_comment, f.file_size 
+	ORDER BY MIN(f.upload_date) DESC";
+
+
 $result = pg_query($connex,$query) or die (pg_last_error() );
 
-										
+
 
 echo "</BR>";
 
