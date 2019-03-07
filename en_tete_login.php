66098a35181438768448dcadc87a77852c58cc7c
AJambon <aurelie.jambon@agro-bordeaux.fr>
2019-03-07 11:48:48 +0100

    création d'un nouvel en-tête spécifique pour la page login sans le bouton d'accès à page d'accueil

---
+++ en_tete_login.php
@@ -0,0 +1,21 @@
+<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<link rel="stylesheet" href="css/custom.css">
+		</head>
+
+		<body>
+
+			
+				<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+					<img src="picto\geosys_logo.png" class="rounded mx-auto d-block" alt="Responsive image" width="250px" height ="125px">
+					<button class="btn btn-primary" onclick="location.href='US_1.21_account_monitoring_by_user.php'" >
+					<img src="picto\person.png" class="rounded float-right" alt="Responsive image" width="75px" height ="75px">
+					</button>
+					
+				</nav>
+		</body>
+</html>
