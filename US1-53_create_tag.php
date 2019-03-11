7bf7a83085d0c3a35f33753cd34fb89389509428
Julien Lorriette <julien.lorriette@agro-bordeaux.fr>
2019-03-11 17:36:13 +0100

    Bootstraped and homogeneized pages
    Also solved a bug in 1_54_modify tag type

--- US1-53_create_tag.php
+++ US1-53_create_tag.php
@@ -25,42 +25,45 @@
             }
 
         </script>
-		
+
 	</head>
 	<body>
 		<?php
 				 include("en_tete.php");
-				 echo "</br>";
+
         ?>
-        
-        <?php 
-            // To add a new client 
+
+        <?php
+            // To add a new client
             require "./tab_donnees/funct_connex.php";
-			echo '<form name="coucou" action="US1-54_manage_tags.php">
+			echo '<div class="container">
+				<form name="coucou" action="US1-54_manage_tags.php">
 				<button name="return" class="btn btn-outline-info" type="submit">Back</button>
 				</form>';
-        ?>
-        <div class="container">
-        <?php
+
             $con=new Connex();
-            $connex=$con->connection;    
+            $connex=$con->connection;
             // parameters of request
             $query = "SELECT id_tag_type, name_tag_type  FROM tag_type
-                GROUP BY id_tag_type ORDER BY name_tag_type"; 
+                GROUP BY id_tag_type ORDER BY name_tag_type";
             // request execution
             // and recuperation of recordset
-            $result = pg_query($connex, $query)  
+            $result = pg_query($connex, $query)
                 or die('Échec de la requête : ' . pg_last_error($connex));
 				echo '<form name="form_creation" action="US1-53_create_tag.php" onsubmit="return validation()" method="get">';
-					echo '<h1 class="card-title" align="center">Add a new tag</h1>';
-					echo '<div class=card-body>';
-					echo '<label>Choose your tag type :</label><br/>
-					<select name="liste_type" >;
-						<option selected="selected">Select a new category for your new tag</option>';
+					echo '<h4 class="card-title">Add a new tag</h4>
+					<div class="input-group mb-3">
+  				<div class="input-group-prepend">
+					<span class="input-group-text"> Choose your tag type :</span></div>
+					<select class="custom-select" name="liste_type" >';
+
+
+
 						while($row = pg_fetch_array($result)){
 							echo '<option value ='.$row["id_tag_type"].'> '.$row["name_tag_type"].' </option>';
 						}
-					echo '</select></br></br>
+					echo '</select></div>
+
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="inputGroup-sizing-default"> Enter the tag name : </span>
@@ -76,32 +79,32 @@
 										</div>
                                         <div><button type="submit" class="btn btn-outline-success">Validate</button></div>
 						</div>
-					
+
 				</form>';
 				?>
 		</div>
 			<?php
-				
-				
-            
+
+
+
             if (isset($_GET["tag_name"])){ //if we click on validate the previous form, we create the tag in the database
                 $con=new Connex();
                 $connex=$con->connection;
-                $query = "SELECT *  FROM tags"; 
-                // request execution 
-                // and recuperation of recordset 
+                $query = "SELECT *  FROM tags";
+                // request execution
+                // and recuperation of recordset
                 $result = pg_query($connex, $query)  or die('Échec de la requête : ' . pg_last_error($connex));
 
                 //Creation of table to insert a new unique id in "tags" table
-                $tab_id_tag=array() ;	
+                $tab_id_tag=array() ;
                 $a=0;
-                while ($row = pg_fetch_array($result)) { 
+                while ($row = pg_fetch_array($result)) {
 					// The access to a table element can be done thanks to index or field name
                     // We can access here by the index
-                    for ($i=1 ; $i <= pg_num_fields($result)-1; $i++)	 	
+                    for ($i=1 ; $i <= pg_num_fields($result)-1; $i++)
                         {$tab_id_tag[$a]=$row[0];
                         $a=$a+1;
-                    } 
+                    }
                 }
                 $min_id= min($tab_id_tag);
                 $new_id=$min_id;
@@ -111,22 +114,22 @@
 
                 //Recuperation of data to insert in the database
                 $id_tag=$new_id;
-                $id_tag_type=$_GET["liste_type"]; 
+                $id_tag_type=$_GET["liste_type"];
                 $tag_name=$_GET["tag_name"];
                 if (isset($_GET["tag_description"]))
                     {$tag_description = $_GET["tag_description"];
                 }
                 else $tag_description= " ";
                 $sql = "INSERT INTO tags VALUES (".$id_tag.",".$id_tag_type." ,'".$tag_name."', '".$tag_description."')";
-                $res = pg_query($connex, $sql)  
+                $res = pg_query($connex, $sql)
                 or die('Échec de la requête : ' . pg_last_error($connex));
-            
-                echo 'New tag '.$tag_name.' created';
+
+                echo '<div class="container"><p> New tag '.$tag_name.' created</p></div>';
 
             }
-       
+
             ?>
-			</div>	
+			</div>
 		</div>
 	</body>
 	<?php
