563845406b02215da0fb180ec7cab8ba8181202c
Elsa Guilley <elsa.guilley@agro-bordeaux.fr>
2019-03-11 16:26:56 +0100

    Creation tag type

--- US1-51_create_tag_type.php
+++ US1-51_create_tag_type.php
@@ -44,7 +44,7 @@
             // and recuperation of recordset 
             $result = pg_query($connex, $query)  
                 or die('Échec de la requête : ' . pg_last_error($connex)); 
-				echo '<form name="form_creation" action="US1-53_create_tag_type.php" onsubmit="return validation()" method="get">'; 
+				echo '<form name="form_creation" action="US1-51_create_tag_type.php" onsubmit="return validation()" method="get">'; 
                 echo '<fieldset style="width: 500px">';                  
 					echo '</select></br>
                                         <div class="input-group mb-3">
@@ -56,56 +56,26 @@
                                         </div>
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" > Enter a description :</span>
+                                                <span class="input-group-text" name="description" > Enter a description :</span>
                                             </div>
                                             <textarea class="form-control" aria-label="With textarea" aria-describedby="inputGroup-sizing-default" name="tag_description"></textarea></br>
 					</div>
-                                        <div><button type="submit" class="btn btn-outline-success">Validate</button></div>
+                                        <div><button type="submit" class="btn btn-outline-success" name="Validate_tag_type">Validate</button></div>
 				</form>';
 				?>
 		</div>
 				<?php
-
-            
-            if (isset($_GET["name_tag_type"])){ //if we click on validate the previous form, we create the tag in the database
-                require_once "funct_connex.php";
+            if(isset($_GET["Validate_tag_type"])){ //if we click on validate the previous form, we create the tag in the database
+				$tag_type_name = $_GET["tag_name"];
+				$tag_type_description = $_GET["description"];
                 $con=new Connex();
                 $connex=$con->connection;
-                $query = "SELECT *  FROM tag_type"; 
-                // request execution
-				// and recuperation of recordset  
+                $query = "INSERT INTO tag_type(name_tag_type, description_tag_type) VALUES ('".$tag_type_name."', '".$tag_type_description."')";
                 $result = pg_query($connex, $query)  or die('Échec de la requête : ' . pg_last_error($connex));
 
-                //Creation of table to insert a new unique id in "tags" table
-                $tab_id_tag_type=array() ;	
-                $a=0;
-                while ($row = pg_fetch_array($result)) { 
-                    // The access to a table element can be done thanks to index or field name
-                    // We can access here by the index 
-                    for ($i=1 ; $i <= pg_num_fields($result)-1; $i++)	 	
-                        {$tab_id_tag_type[$a]=$row[0];
-                        $a=$a+1;
-                    } 
-                }
-                $min_id_type= min($tab_id_tag_type);
-                $new_id_type=$min_id_type;
-                while (in_array($new_id_type, $tab_id_tag_type)){
-                    $new_id_type=$new_id_type+1;
-                }
-
-                //Recuperation of data to insert in the database
-                $id_tag_type=$new_id_type;
-                $name_tag_type=$_GET["name_tag_type"];
-                if (isset($_GET["description_tag_type"]))
-                    {$description_tag_type = $_GET["description_tag_type"];
-                }
-                else $description_tag_type= " ";
-                $sql = "INSERT INTO tag_type VALUES (".$id_tag_type." ,'".$name_tag_type."', '".$description_tag_type."')";
-                $res = pg_query($connex, $sql)  
-                or die('Échec de la requête : ' . pg_last_error($connex));
-            
-                echo 'New type tag '.$name_tag_type.' created';
-
+        
+                echo 'New type tag " <b> '.$tag_type_name.' </b> " created';
+				
             }
                 
             ?>
